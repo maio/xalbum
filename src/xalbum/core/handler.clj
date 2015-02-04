84271ec5edac7de6ab7c12a77a21fae32edbab24
Marian Schubert <marian.schubert@gmail.com>
2015-02-04 08:51:00 +0100

    get rid of useless wrapper functions

--- src/xalbum/core/handler.clj
+++ src/xalbum/core/handler.clj
@@ -26,34 +26,21 @@
                                [:a] (html/set-attr :href (:url album))
                                [:img] (html/set-attr :src (:teaser-url album))))
 
-(defn render-photo [photo-file]
-  (file-response photo-file))
-
-(defn render-photo-thumb [photo-file]
-  (file-response (photo/resize-to-fit photo-file {:width nil :height 300})))
-
-(defn render-teaser [teaser-file]
-  (file-response teaser-file))
-
-(defn render-album [storage album-id]
-  (album-template album-id (data/get-album-photos storage album-id)))
-
-(defn render-main [storage]
-  (main-template (data/get-albums storage)))
-
 (defn build-app [storage]
   (defroutes app-routes
-    (GET "/" [] (render-main storage))
+    (GET "/" [] (main-template (data/get-albums storage)))
 
-    (GET "/album/:album-id" [album-id] (render-album storage album-id))
+    (GET "/album/:album-id" [album-id]
+         (album-template album-id (data/get-album-photos storage album-id)))
     (GET "/album/:album-id/teaser.jpg" [album-id]
-         (render-teaser (data/get-teaser-location storage album-id)))
+         (file-response (data/get-teaser-location storage album-id)))
     (GET "/album/:album-id/:photo-filename" [album-id photo-filename]
-         (render-photo (data/get-photo-location storage album-id photo-filename)))
+         (file-response (data/get-photo-location storage album-id photo-filename)))
     ;; cache generated thumbs forever on client & proxy
     (photo/wrap-cache-forever
      (GET "/album/:album-id/thumb/:photo-filename" [album-id photo-filename]
-          (render-photo-thumb (data/get-photo-location storage album-id photo-filename))))
+          (let [photo-file (data/get-photo-location storage album-id photo-filename)]
+            (file-response (photo/resize-to-fit photo-file {:width nil :height 300})))))
 
     (route/not-found "Not Found"))
   (wrap-defaults app-routes site-defaults))
